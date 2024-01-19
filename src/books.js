620f2701ea942ab5eb02a9b4075b3361279c3372
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-19 16:39:31 +0100

    still struggling

--- src/books.js
+++ src/books.js
@@ -1,63 +1,81 @@
 // Iteration 1 | Books Array
 
-// Book 1
-  // title: The Old Man and the Sea
-  // pages: 128
-  // author: Ernest Hemingway
-  // details: {
-  //    language: English
-  //    description: One of Hemingway's most famous works, it tells the story of Santiago...
-  // }
+const booksArray = [
+  {
+      title: 'The Old Man and the Sea',
+      pages: 128,
+      author: 'Ernest Hemingway',
+      details: {
+        language: 'english',
+        description: 'One of Hemingway\'s most famous works, it tells the story of Santiago...'
+      }
+    },
+  {
+      title: 'The Airbnb Story',
+      pages: 256,
+      author: 'Leight Gallagher',
+      details: {
+        language: 'English',
+        description: 'This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...'
+      }
+    },
 
-// Book 2
-  // title: The Airbnb Story
-  // pages: 256
-  // author: Leight Gallagher
-  // details: {
-  //    language: English
-  //    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-  // }
+  {
+      title: 'Pride and Prejudice',
+      pages: 279,
+      author: 'Jane Austen',
+      details: {
+        language: 'english',
+        description: 'One of the most popular novels in the English language...'
+      } 
+    },
 
-// Book 3
-  // title: Pride and Prejudice
-  // pages: 279
-  // author: Jane Austen
-  // details: {
-  //    language: English
-  //    description: One of the most popular novels in the English language...
-  // }
+  {
+      title: 'Educated - A Memoir',
+      pages: 352,
+      author: 'Tara Westover',
+      details: {
+        language: 'english',
+        description: 'Educated is an account of the struggle for self-invention...',
+      }
+    },
 
-// Book 4
-  // title: Educated - A Memoir
-  // pages: 352
-  // author: Tara Westover
-  // details: {
-  //    language: English
-  //    description: Educated is an account of the struggle for self-invention...
-  // }
-
-// Book 5
-  // title: The Art of Learning
-  // pages: 288
-  // author: Josh Waitzkin
-  // details: {
-  //    language: English
-  //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-  // }
-
-
-// Your code here:
-const booksArray = [];
-
-
-
+  {
+      title: 'The Art of Learning',
+      pages: 288,
+      author: 'Josh Waitzkin',
+      details: {
+        language: 'english',
+        description: 'The Art of Learning takes readers through Waitzkins unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.'
+      }
+    },
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
 
+function getBookDetails(book) {
+  let bookTitle= '';
+  let bookAuthor= '';
+  let bookPages= 0;
+
+  for (let i=0; i<booksArray.length; i++){
+
+    if (booksArray[i].title===book){
+    bookTitle= booksArray[i].title;
+    bookAuthor= booksArray[i].author;
+    bookPages= booksArray[i].pages;
+    }
+  }
+  console.log(`${bookTitle} - ${bookAuthor} - ${bookPages} pages.`);
 }
 
+function deleteLanguage(bookArray) {
+  for (let i=0; i<booksArray.length; i++){
+    if ('details' in booksArray[i] && 'language' in booksArray[i].details){
+      delete booksArray[i].details.language;
+    } 
+  }
+}
 
 
 // Iteration 3 | Delete Language
