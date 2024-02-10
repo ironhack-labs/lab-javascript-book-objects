68567627c577b0b80baa2e41e823c9d077963c27
Shannon <shannonblas@pucpr.edu>
2024-02-10 15:17:19 -0500

    done

--- src/books.js
+++ src/books.js
@@ -1,109 +1,100 @@
-// Iteration 1 | Books Array
+const booksArray = [ //Array of Objects 
 
-// Book 1
-  // title: The Old Man and the Sea
-  // pages: 128
-  // author: Ernest Hemingway
-  // details: {
-  //    language: English
-  //    description: One of Hemingway's most famous works, it tells the story of Santiago...
-  // }
+  book1 = {
+    title: "The Old Man and the Sea",
+    pages: "128",
+    author: " Ernest Hemingway",
+    details: {
+      language: " English",
+      description: " One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
 
-// Book 2
-  // title: The Airbnb Story
-  // pages: 256
-  // author: Leight Gallagher
-  // details: {
-  //    language: English
-  //    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-  // }
+  book2 = {
+    title: "The Airbnb Story",
+    pages: " 256",
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  book3 = {
+    title: "Pride and Prejudice",
+    pages: "279",
+    author: "Jane Austen",
+    details: {
+      language: "English",
+      description: "One of the most popular novels in the English language...",
+    }
+  },
 
-// Book 3
-  // title: Pride and Prejudice
-  // pages: 279
-  // author: Jane Austen
-  // details: {
-  //    language: English
-  //    description: One of the most popular novels in the English language...
-  // }
+  book4 = {
+    title: " Educated - A Memoir",
+    pages: "352",
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: " Educated is an account of the struggle for self - invention...",
+    }
+  },
 
-// Book 4
-  // title: Educated - A Memoir
-  // pages: 352
-  // author: Tara Westover
-  // details: {
-  //    language: English
-  //    description: Educated is an account of the struggle for self-invention...
-  // }
+  book5 = {
+    title: "The Art of Learning",
+    pages: "288",
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  },
+];
 
-// Book 5
-  // title: The Art of Learning
-  // pages: 288
-  // author: Josh Waitzkin
-  // details: {
-  //    language: English
-  //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-  // }
+// console.log(booksArray[4]);
+// console.log(booksArray[1].title);
+// console.log(booksArray.length); //5 objects 
 
+function getBookDetails(bookObject) {
 
-// Your code here:
-const booksArray = [];
+  for (let i = 0; i < booksArray.length; i++) {
 
+    let bookObject = ""; //empty string 
 
+    bookObject = bookObject + (`${booksArray[i].title} - ${booksArray[i].author} - ${booksArray[i].pages} pages}`)
 
+  }
 
-// Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+  return bookObject;
 }
 
+// console.log(getBookDetails(book2));
 
 
-// Iteration 3 | Delete Language
-// Your code here:
+//Iteration 3 | Delete language 
+//Delete the nested object property "language" from each book object 
 
 
+console.log(booksArray[2].details.language); //accessing language (The property I want to delete)
 
-
-// Iteration 4 | Estimated Reading Time
-// Your code here:
-
-
-
-
-// Bonus: Iteration 5 | Books Dictionary
-
-/* The `dictionary` is an object containing books grouped by author. 
- The book info is stored in arrays with structure: [title, pages]. 
-*/
-const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
-};
-
-function booksByAuthor() {
-  // Your code here:
-  
+for (let i = 0; i < booksArray[i].details.language; i++) {
+  delete booksArray[i].details.language;
 }
 
+//Iteration 4 | Estimated Reading TIme 
+//Add a new property *readingTime* to each object in the booksArray
+
+//Notes 
+//keys are always strings
+//bracket notation must pass as string
+
+//add a value to a key just using dot notation 
+
+booksArray.readingTime;
+//Iterate over the array and add a new property: We need a loop 
 
 
-// Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
-  // Your code here:
-  
+console.log(booksArray[0]);
+
+for (let i = 0; i < booksArray.length; i++) {
+  console.log(i); // 0,1,2,3,4 
 }
