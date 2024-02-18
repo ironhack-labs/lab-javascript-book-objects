4ed9a379c1b141a14368b45cb08c79ceda7a04f3
Memoryman2022 <rlacannon@gmail.com>
2024-02-18 11:58:39 +0100

    solved lab

--- src/books.js
+++ src/books.js
@@ -1,59 +1,59 @@
-// Iteration 1 | Books Array
+// // Iteration 1 | Books Array
 
-// Book 1
-//   title: The Old Man and the Sea
-//   pages: 128
-//   author: Ernest Hemingway
-//   details: {
-//      language: English
-//      description: One of Hemingway's most famous works, it tells the story of Santiago...
-//   }
+// // Book 1
+// //   title: The Old Man and the Sea
+// //   pages: 128
+// //   author: Ernest Hemingway
+// //   details: {
+// //      language: English
+// //      description: One of Hemingway's most famous works, it tells the story of Santiago...
+// //   }
 
-// Book 2
-//   title: The Airbnb Story
-//   pages: 256
-//   author: Leight Gallagher
-//   details: {
-//      language: English
-//      description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-//   }
+// // Book 2
+// //   title: The Airbnb Story
+// //   pages: 256
+// //   author: Leight Gallagher
+// //   details: {
+// //      language: English
+// //      description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
+// //   }
 
-// Book 3
-//   title: Pride and Prejudice
-//   pages: 279
-//   author: Jane Austen
-//   details: {
-//      language: English
-//      description: One of the most popular novels in the English language...
-//   }
+// // Book 3
+// //   title: Pride and Prejudice
+// //   pages: 279
+// //   author: Jane Austen
+// //   details: {
+// //      language: English
+// //      description: One of the most popular novels in the English language...
+// //   }
 
-// Book 4
-//   title: Educated - A Memoir
-//   pages: 352
-//   author: Tara Westover
-//   details: {
-//      language: English
-//      description: Educated is an account of the struggle for self-invention...
-//   }
+// // Book 4
+// //   title: Educated - A Memoir
+// //   pages: 352
+// //   author: Tara Westover
+// //   details: {
+// //      language: English
+// //      description: Educated is an account of the struggle for self-invention...
+// //   }
 
-// Book 5
-//   title: The Art of Learning
-//   pages: 288
-//   author: Josh Waitzkin
-//   details: {
-//      language: English
-//      description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-//   }
+// // Book 5
+// //   title: The Art of Learning
+// //   pages: 288
+// //   author: Josh Waitzkin
+// //   details: {
+// //      language: English
+// //      description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
+// //   }
 
 
-// const testArray = {books:[
-//   {name:"book 1", title: 'Bla'},
-//   {name:"book 2", title: 'Bla2'},
-//   {name:"book 3", title: 'Bla3'}
+// // const testArray = {books:[
+// //   {name:"book 1", title: 'Bla'},
+// //   {name:"book 2", title: 'Bla2'},
+// //   {name:"book 3", title: 'Bla3'}
 
-// ]}
+// // ]}
 
-// console.log(testArray.books[0].name)
+// // console.log(testArray.books[0].name)
 
 // Your code here:
 const booksArray = [
@@ -149,7 +149,7 @@ function addDetail(booksArray){
       book.readingTime = readingTime
     }
   })
-}``
+}
 addDetail(booksArray)
 console.log(booksArray)
 
@@ -160,33 +160,39 @@ console.log(booksArray)
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
-const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
-};
+// const dictionary = {
+//     "J. K. Rowling": [
+//         ["Harry Potter and the Philosopher's Stone", 223],
+//         ["Harry Potter and the Chamber of Secrets", 251],
+//         ["Harry Potter and the Prisoner of Azkaban", 317],
+//         ["Harry Potter and the Goblet of Fire", 636],
+//     ],
+//     "Neal Stephenson": [
+//         ["Cryptonomicon", 928],
+//         ["Anathem", 1008],
+//         ["Fall; or, Dodge in Hell", 896],
+//     ],
+//     "Malcolm Gladwell": [
+//         ["Outliers", 320],
+//         ["Blink", 287],
+//     ],
+// };
 
-function booksByAuthor() {
-  // Your code here:
+// function booksByAuthor(dictionary) {
+//   // Your code here:
+//   for (let i = 0; i < dictionary.length; i++){
+    
+//   }
+//   //dictionary.object.keys()
+//   console.log
+//   return Object.keys(dictionary)
+
+// }
+// booksByAuthor(dictionary)
+
+
+// // Bonus: Iteration 6 | Average Page Count
+// function averagePageCount() {
+//   // Your code here:
   
-}
-
-
-
-// Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
-  // Your code here:
-  
-}
+// }
