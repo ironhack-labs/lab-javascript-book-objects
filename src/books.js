70c9e9c1e803c667f9a40fe0320bfee32965af22
Brenni1 <phillip.brenndoerfer@gmail.com>
2024-02-18 12:59:54 +0100

    Finished the Lab

--- src/books.js
+++ src/books.js
@@ -1,75 +1,80 @@
 // Iteration 1 | Books Array
 
-// Book 1
-  // title: The Old Man and the Sea
-  // pages: 128
-  // author: Ernest Hemingway
-  // details: {
-  //    language: English
-  //    description: One of Hemingway's most famous works, it tells the story of Santiago...
-  // }
 
-// Book 2
-  // title: The Airbnb Story
-  // pages: 256
-  // author: Leight Gallagher
-  // details: {
-  //    language: English
-  //    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-  // }
-
-// Book 3
-  // title: Pride and Prejudice
-  // pages: 279
-  // author: Jane Austen
-  // details: {
-  //    language: English
-  //    description: One of the most popular novels in the English language...
-  // }
-
-// Book 4
-  // title: Educated - A Memoir
-  // pages: 352
-  // author: Tara Westover
-  // details: {
-  //    language: English
-  //    description: Educated is an account of the struggle for self-invention...
-  // }
-
-// Book 5
-  // title: The Art of Learning
-  // pages: 288
-  // author: Josh Waitzkin
-  // details: {
-  //    language: English
-  //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-  // }
 
 
 // Your code here:
-const booksArray = [];
+
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+   }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+   }
+  },
+  {
+    title: "Pride and Prejudice",
+    pages: 279,
+    author: "Jane Austen",
+    details: {
+      language: "English",
+      description: "One of the most popular novels in the English language...",
+   }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention...",
+   }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+   }
+  }
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+return `${book.title} - ${book.author} - ${book.pages} pages`
 }
-
-
-
+console.log(getBookDetails(booksArray[4]));
 // Iteration 3 | Delete Language
 // Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language
+};
 
-
-
+console.log(booksArray);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
-
+booksArray.forEach((book) => {
+  book.readingTime = Math.ceil((book.pages * 500) / 90);
+});
 
 
 // Bonus: Iteration 5 | Books Dictionary
@@ -95,15 +100,53 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
-  // Your code here:
-  
+
+
+function booksByAuthor(dictionary) {
+  let booksArray = [];
+
+  for (key in dictionary) {
+    Object.values(dictionary[key]).forEach((element) => {
+      booksArray.push({
+        title: element[0],
+        pages: element[1],
+        author: key,
+      });
+    });
+  }
+  return booksArray;
 }
 
+const dictionaryExample = {
+  "J. K. Rowling": [
+      ["Harry Potter and the Philosopher's Stone", 223],
+      ["Harry Potter and the Chamber of Secrets", 251],
+      ["Harry Potter and the Prisoner of Azkaban", 317],
+      ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+      ["Cryptonomicon", 928],
+      ["Anathem", 1008],
+      ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+      ["Outliers", 320],
+      ["Blink", 287],
+  ],
+};
+
+console.log(booksByAuthor(dictionaryExample));
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(array) {
   // Your code here:
+  let sumPages = 0;
   
+  array.forEach((book)=>{
+    sumPages += book.pages
+
+  })
+    
+  return sumPages / array.length;
 }
