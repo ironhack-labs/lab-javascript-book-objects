516e32626448b9345b0573f2412254a27ff845aa
John Doe <johndoe@example.com>
2024-03-13 18:17:43 +0100

    books

--- src/books.js
+++ src/books.js
@@ -1,162 +1,160 @@
 // Iteration 1 | Books Array
 
 // Book 1
-  // title: The Old Man and the Sea
-  // pages: 128
-  // author: Ernest Hemingway
-  // details: {
-  //    language: English
-  //    description: One of Hemingway's most famous works, it tells the story of Santiago...
-  // }
+// title: The Old Man and the Sea
+// pages: 128
+// author: Ernest Hemingway
+// details: {
+//    language: English
+//    description: One of Hemingway's most famous works, it tells the story of Santiago...
+// }
 
 // Book 2
-  // title: The Airbnb Story
-  // pages: 256
-  // author: Leight Gallagher
-  // details: {
-  //    language: English
-  //    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-  // }
+// title: The Airbnb Story
+// pages: 256
+// author: Leight Gallagher
+// details: {
+//    language: English
+//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
+// }
 
 // Book 3
-  // title: Pride and Prejudice
-  // pages: 279
-  // author: Jane Austen
-  // details: {
-  //    language: English
-  //    description: One of the most popular novels in the English language...
-  // }
+// title: Pride and Prejudice
+// pages: 279
+// author: Jane Austen
+// details: {
+//    language: English
+//    description: One of the most popular novels in the English language...
+// }
 
 // Book 4
-  // title: Educated - A Memoir
-  // pages: 352
-  // author: Tara Westover
-  // details: {
-  //    language: English
-  //    description: Educated is an account of the struggle for self-invention...
-  // }
+// title: Educated - A Memoir
+// pages: 352
+// author: Tara Westover
+// details: {
+//    language: English
+//    description: Educated is an account of the struggle for self-invention...
+// }
 
 // Book 5
-  // title: The Art of Learning
-  // pages: 288
-  // author: Josh Waitzkin
-  // details: {
-  //    language: English
-  //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-  // }
-
+// title: The Art of Learning
+// pages: 288
+// author: Josh Waitzkin
+// details: {
+//    language: English
+//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
+// }
 
 // Your code here:
-// index.js
-
-// Define the books array
 const booksArray = [
   {
-      title: "The Old Man and the Sea",
-      pages: 128,
-      author: "Ernest Hemingway",
-      description: "One of Hemingway's most famous works, it tells the story of Santiago...",
-      language: "English"
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: `English`,
+      description: `One of Hemingway's most famous works, it tells the story of Santiago...`,
+    },
   },
   {
-      title: "The Airbnb Story",
-      pages: 256,
-      author: "Leight Gallagher",
-      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
-      language: "English"
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: `English`,
+      description: `One of Hemingway's most famous works, it tells the story of Santiago...`,
+    },
   },
   {
-      title: "Pride and Prejudice",
-      pages: 279,
-      author: "Jane Austen",
-      description: "One of the most popular novels in the English language...",
-      language: "English"
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: `English`,
+      description: `One of Hemingway's most famous works, it tells the story of Santiago...`,
+    },
   },
   {
-      title: "Educated - A Memoir",
-      pages: 352,
-      author: "Tara Westover",
-      description: "Educated is an account of the struggle for self-invention...",
-      language: "English"
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: `English`,
+      description: `One of Hemingway's most famous works, it tells the story of Santiago...`,
+    },
   },
   {
-      title: "The Art of Learning",
-      pages: 288,
-      author: "Josh Waitzkin",
-      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
-      language: "English"
-  }
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: `English`,
+      description: `One of Hemingway's most famous works, it tells the story of Santiago...`,
+    },
+  },
 ];
 
-// Export the books array
-
 // Iteration 2 | Book Details
-
-function getBookDetails(book){
-
-return `${book.title} - ${book.pages} - ${book.author}`
-
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
-
- book = booksArray[4];
-
-console.log(getBookDetails(book));
-
 // Iteration 3 | Delete Language
-// Your code here:
+booksArray.forEach((book) => {
+  delete book.details.language;
+});
+console.log("Our books array", booksArray);
 
-booksArray.forEach(book => {
-
-  delete book.language;
-
-})
-
-console.log(booksArray);
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-booksArray.forEach(book => {
-
-const readingTimeMinutes = math.round((book.pages 500 * ) / 90)
-
-book.readingTime = readingTimeMinutes;
-})
-
-console.log(booksArray);
-book
+function addReadingTime(bookObj) {
+  const readingTime = Math.ceil((bookObj.pages * 500) / 90);
+  bookObj.readingTime = readingTime;
+}
+booksArray.forEach((bookObj) => {
+  addReadingTime(bookObj);
+});
 // Bonus: Iteration 5 | Books Dictionary
 
 /* The `dictionary` is an object containing books grouped by author.
  The book info is stored in arrays with structure: [title, pages].
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
-  // Your code here:
-
+function booksByAuthor(dict) {
+  const booksArr = [];
+  for (let author in dict) {
+    for (let book of dict[author]) {
+      let bookObject = {
+        title: book[0],
+        pages: book[1],
+        author: author,
+      };
+      booksArr.push(bookObject);
+    }
+  }
+  return booksArr;
 }
 
-
+let allBooks = booksByAuthor(dictionary);
 
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-
 }
