fa6683efdfb0665cae386d7542ebdcdcceac7deb
Alexandre Cabral <alex.miguel89@gmail.com>
2024-04-12 14:59:46 +0100

    done

--- src/books.js
+++ src/books.js
@@ -9,6 +9,8 @@
 //    description: One of Hemingway's most famous works, it tells the story of Santiago...
 // }
 
+
+
 // Book 2
 // title: The Airbnb Story
 // pages: 256
@@ -28,8 +30,8 @@
 // }
 
 // Book 4
-// title: The Art of Learning
-// pages: 288
+// title: "The Art of Learning"
+// pages: "288
 // author: Josh Waitzkin
 // details: {
 //    language: English
@@ -38,27 +40,77 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+    },
+    readingTime: Math.ceil(128 * 500 / 90),
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+
+    },
+    readingTime: Math.ceil(256 * 500 / 90),
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention...",
+
+    },
+    readingTime: Math.ceil(352 * 500 / 90),
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    },
+    readingTime: Math.ceil(288 * 500 / 90),
+  }
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`
 }
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
+booksArray.forEach(book => {
+  if (book.details && book.details.language) {
+    delete book.details.language
+  }
+})
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+
+     
+  
 
 
 
