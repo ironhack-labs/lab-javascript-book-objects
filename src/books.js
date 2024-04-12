1a9e1484e62d953e67d66ae87d077e4324fc9613
Ignacio Monzón Acosta <ignaciomonzona@gmail.com>
2024-04-12 20:53:44 +0200

    Realizados los 4 primeros

--- src/books.js
+++ src/books.js
@@ -38,14 +38,48 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [ {title: "The Old Man and the Sea", 
+                           pages: 128, 
+                           author: "Ernest Hemingway", 
+                           details: { language: "English", 
+                                      description: "One of Hemingway's most famous works, it tells the story of Santiago...", 
+                                    }
+                          } , 
 
+                  {title: "The Airbnb Story",
+                             pages: 256,
+                             author: "Leight Gallagher", 
+                             details: { language: "English", 
+                                        description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb.."
+                                      }
+                            }, 
+
+                   { title: "Educated - A Memoir", 
+                             pages: 352,
+                             author: "Tara Westover", 
+                             details: {language: "English", 
+                             description: "Educated is an account of the struggle for self-invention...", 
+                                      }
+                           }, 
+
+                   {title: "The Art of Learning", 
+                            pages: 288, 
+                            author: "Josh Waitzkin", 
+                            details: {language: "English", 
+                                      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+                                     }
+                    }
+                  ]
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(books) {
   // Your code here:
+
+let {title, author, pages} = book; 
+
+return `${title} ${author} ${pages} pages`;
 
 }
 
@@ -54,11 +88,26 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
-
+for (const book of booksArray) {
+  delete book.details.language; 
+}
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+
+
+
+  booksArray.forEach(book => { 
+
+  const totalWords = book.pages * 500;  
+  const time = Math.floor(totalWords / 90); 
+  books.readingTime = time;                                
+}); 
+
+
+console.log(booksArray); 
+
 
 
 
