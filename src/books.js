0a062dd972d9b1bab5317ffc59c0dd2449773183
Manuele Vancin <mfvancin@gmail.com>
2024-04-12 15:39:48 +0100

    LabDay5

--- src/books.js
+++ src/books.js
@@ -38,27 +38,59 @@
 
 
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+      title: "The Old Man and the Sea",
+      pages: 128,
+      author: "Ernest Hemingway",
+      details: {
+          language: "English",
+          description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+      }
+  },
+  {
+      title: "The Airbnb Story",
+      pages: 256,
+      author: "Leight Gallagher",
+      details: {
+          language: "English",
+          description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+      }
+  },
+  {
+      title: "Educated - A Memoir",
+      pages: 352,
+      author: "Tara Westover",
+      details: {
+          language: "English",
+          description: "Educated is an account of the struggle for self-invention..."
+      }
+  },
+  {
+      title: "The Art of Learning",
+      pages: 288,
+      author: "Josh Waitzkin",
+      details: {
+          language: "English",
+          description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out"
+       }
+   }
+    
+]
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+    return book.title.author.pages
 }
-
+  
 
 
 // Iteration 3 | Delete Language
 // Your code here:
 
-
-
-
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+let readingTime;
 
 
 
