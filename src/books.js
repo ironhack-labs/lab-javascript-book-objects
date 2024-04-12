8941934c120d42c742938b7719a27a1539529fd3
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-04-12 14:30:09 +0200

    done

--- src/books.js
+++ src/books.js
@@ -38,27 +38,71 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  }
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(bookObject) {
   // Your code here:
-
+  return `${bookObject.title} - ${bookObject.author} - ${bookObject.pages} pages`
 }
+
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
+for (element of  booksArray) {
+  delete element.details.language
+};
 
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+for (element of booksArray){
+  element.readingTime = Math.ceil((element.pages * 500)/90);
+}
 
 
 
@@ -69,32 +113,48 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionaryObject) {
   // Your code here:
-  
-}
+  arrayBooks = [];
+  for (key in dictionaryObject){
+    for (i of dictionaryObject[key]){
+      authorObject = {};
+      authorObject.title = i[0];
+      authorObject.pages = i[1];
+      authorObject.author = key;
+      arrayBooks.push(authorObject);
+    };
+  };
+  return arrayBooks;
+};
+
 
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(arrayBooks) {
   // Your code here:
-  
-}
+  numberPages = 0;
+  for (i of arrayBooks){
+    numberPages += i.pages;
+  };
+  averageTimePage = Math.ceil(numberPages/arrayBooks.length);
+  return averageTimePage;
+};
