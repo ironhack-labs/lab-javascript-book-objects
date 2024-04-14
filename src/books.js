0c5724a0eb4e1d61f41fec3b344354ac03bb21e8
CristinaColomoiets <colomoetscristina@gmail.com>
2024-04-14 13:16:16 +0200

    lab is done

--- src/books.js
+++ src/books.js
@@ -1,64 +1,76 @@
 // Iteration 1 | Books Array
 
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
-
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
-
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  Book1 = {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  Book2 = {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  Book3 = {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
 
+  },
+  Book4 = {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    },
+  }
+];
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book_obj){
+  
+  let info_about_book = `${book_obj.title} - ${book_obj.author} - ${book_obj.pages} pages`;
+  return info_about_book;
 }
+let info_one_book = getBookDetails(booksArray);
 
 
 
 // Iteration 3 | Delete Language
-// Your code here:
-
-
+function deleteProperty(arr_book){
+  arr_book.forEach((one_book)=>{
+    delete one_book.details.language;
+  })
+}
+let new_books_array = deleteProperty(booksArray);
 
 
 // Iteration 4 | Estimated Reading Time
-// Your code here:
+function addProperty(arr_book){
+  arr_book.forEach((each_book)=>{
+    each_book.readingTime = Math.ceil((each_book.pages * 500) / 90);
+  })
+}
+
+let upgraded_books = addProperty(booksArray);
 
 
 
@@ -66,7 +78,7 @@ function getBookDetails() {
 // Bonus: Iteration 5 | Books Dictionary
 
 /* The `dictionary` is an object containing books grouped by author. 
- The book info is stored in arrays with structure: [title, pages]. 
+   The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
     "J. K. Rowling": [
@@ -86,11 +98,10 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
-  // Your code here:
+function booksByAuthor(dictionary_param) {
+  
   
 }
-
 
 
 // Bonus: Iteration 6 | Average Page Count
