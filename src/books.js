039c6ba65e69a9acc142c97cd4077e53cc61c30d
Santi Mera <santigomero@gmail.com>
2024-06-07 15:58:18 +0300

    FT_RMT_27052024 Santiago

--- src/books.js
+++ src/books.js
@@ -1,65 +1,66 @@
 // Iteration 1 | Books Array
 
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
-
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
-
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
-
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  { title: "The Old Man and the Sea",
+   pages: 128,
+   author: "Ernest Hemingway",
+     details: {
+       language: "English",
+       description: "One of Hemingway's most famous works, it tells the story of Santiago..."}
+   },
 
+   { title: "The Airbnb Story",
+ pages: 256,
+ author: "Leight Gallagher",
+details: {
+   language: "English",
+   description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."}
+},
+ {title: "Educated - A Memoir",
+ pages: 352,
+ author: "Tara Westover",
+   details: {
+     language: "English",
+     description: "Educated is an account of the struggle for self-invention..."}
+},
 
+ {title: "The Art of Learning",
+ pages: 288,
+author: "Josh Waitzkin",
+details: {
+language: "English",
+description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."}
+},
+];
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`
 }
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
+  booksArray.forEach(function (bookObject) {
+    delete bookObject.details.language;
+  });
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
+booksArray.forEach(function (readTime){
+  readTime.readingTime = 0;
+});
+
+booksArray.forEach(function(averagePageCount){
+  averagePageCount.readingTime = Math.ceil((averagePageCount.pages * 500) / 90);
+});
 
 
 
@@ -86,15 +87,40 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+
   // Your code here:
-  
-}
+  function booksByAuthor(dictionary) {
+    // Your code here:
+    let bookArr = [];
+    let authors = Object.keys(dictionary);
+    for (let i =0; i<authors.length; i++){
+      for (let j =0; j< dictionary[authors[i]].length; j++){
+        const bookObj = {};
+        bookObj.author = authors[i];
+        bookObj.title = dictionary[authors[i]][j][0];
+        bookObj.pages = dictionary[authors[i]][j][1];
+        bookArr.push(bookObj);
+      }
+    }
+    return bookArr
+  }
+
+
 
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(bookArr1) {
   // Your code here:
+  let sum = 0;
+  let totalBooks = bookArr1.length;
+
+  bookArr1.forEach(function(currentBookPages){
+    sum += currentBookPages.pages;
+  }) 
   
+  let average = sum /(totalBooks);
+
+  return average;
+
 }
