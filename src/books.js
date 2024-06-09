fdeb4cc87aee470adb2903d817cca709f6b281ec
Najma Javed <najma.javed@outlook.com>
2024-06-09 22:33:46 +0200

    Lab completed: lab-javascript-book-objects

--- src/books.js
+++ src/books.js
@@ -38,28 +38,73 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+// Book 1
+{
+title: "The Old Man and the Sea",
+pages: 128,
+author: "Ernest Hemingway",
+details: {
+language: "English",
+description: "One of Hemingway's most famous works, it tells the story of Santiago..."}},
 
+//Book 2
+{
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+  language: "English",
+  description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."}},
 
+  // Book 3
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention..."}},
+ // Book 04
+  {
+      title: "The Art of Learning",
+      pages: 288,
+      author: "Josh Waitzkin",
+      details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+}}
+];
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
+
+    console.log(`${book.title} - ${book.author} - ${book.pages} pages`)
+    return `${book.title} - ${book.author} - ${book.pages} pages`;
 
 }
 
-
-
 // Iteration 3 | Delete Language
 // Your code here:
+booksArray.forEach(function (book){
+  delete book.details.language;
+  console.log(booksArray);
+
+});
 
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+booksArray.forEach(function (book){
 
+  book.readingTime  = Math.ceil(book.pages * 500/90);
+  console.log(booksArray);
+
+});
 
 
 
@@ -86,7 +131,7 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(myDictionary ) {
   // Your code here:
   
 }
