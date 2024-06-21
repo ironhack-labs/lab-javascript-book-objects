d9a68cba0076e46cf12e94cf2d70ef2f4f0d943a
suli <soulaimaneltoumi@gmail.com>
2024-06-21 16:31:48 +0200

    solved lab

--- src/books.js
+++ src/books.js
@@ -1,67 +1,75 @@
 // Iteration 1 | Books Array
 
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
+Book1 = {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description:
+      "One of Hemingway's most famous works, it tells the story of Santiago...",
+  },
+};
+Book2 = {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+    language: "English",
+    description:
+      "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+  },
+};
 
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
+Book3 = {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention...",
+  },
+};
 
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
+Book4 = {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description:
+      "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+  },
+};
 
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [Book1, Book2, Book3, Book4];
+console.log(booksArray);
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+  const details =
+    book.title + " - " + book.author + " - " + book.pages + " pages";
+  return details;
 }
 
-
+console.log(getBookDetails(Book1));
 
 // Iteration 3 | Delete Language
 // Your code here:
+booksArray.forEach((a) => {
+  delete a.details.language;
+});
 
-
-
+console.log(booksArray);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+booksArray.forEach((element) => {
+  element.readingTime = Math.ceil((element.pages * 500) / 90);
+});
 
-
-
+console.log(Book1);
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +77,28 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
 function booksByAuthor() {
   // Your code here:
-  
 }
-
-
 
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-  
 }
