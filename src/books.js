4716ef559ca38d37e23be7550eeb77746f0921be
smammc <smammc@hotmail.com>
2024-07-12 15:57:41 +0100

    Fineshed lab

--- src/books.js
+++ src/books.js
@@ -9,6 +9,17 @@
 //    description: One of Hemingway's most famous works, it tells the story of Santiago...
 // }
 
+book1 = {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description:
+      "One of Hemingway's most famous works, it tells the story of Santiago...",
+  },
+};
+
 // Book 2
 // title: The Airbnb Story
 // pages: 256
@@ -17,6 +28,17 @@
 //    language: English
 //    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
 // }
+
+book2 = {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+    language: "English",
+    description:
+      "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+  },
+};
 
 // Book 3
 // title: Educated - A Memoir
@@ -27,6 +49,17 @@
 //    description: Educated is an account of the struggle for self-invention...
 // }
 
+book3 = {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+    language: "English",
+    description:
+      " Educated is an account of the struggle for self-invention...",
+  },
+};
+
 // Book 4
 // title: The Art of Learning
 // pages: 288
@@ -36,32 +69,39 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
+book4 = {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description:
+      "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+  },
+};
 
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [book1, book2, book3, book4];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
-
-
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
-
+booksArray.forEach((book) => {
+  delete book.details.language;
+  console.log(booksArray);
+});
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
-
+booksArray.forEach((book) => {
+  book.readingTime = Math.ceil((book.pages * 500) / 90);
+});
+console.log(booksArray);
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +109,47 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
-  // Your code here:
-  
+function booksByAuthor(dict) {
+  let result = [];
+  Object.keys(dict).forEach((author) => {
+    for (let book = 0; book < dict[author].length; book++) {
+      authorDict = {
+        title: dict[author][book][0],
+        pages: dict[author][book][1],
+        author: author,
+      };
+      result.push(authorDict);
+    }
+  });
+  return result;
 }
 
-
-
+booksArray;
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(booksArray) {
   // Your code here:
-  
+  let sumPages = 0;
+  let countBooks = 0;
+  booksArray.forEach((book) => {
+    sumPages += book.pages;
+    countBooks++;
+  });
+  return sumPages / countBooks;
 }
