b98d9b0634d960640a50aee0bb845a36b55a051d
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-01 08:36:29 +0200

    Solved lab

--- src/books.js
+++ src/books.js
@@ -38,23 +38,68 @@
 
 
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+    },
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leigh Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+    },
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention...",
+    },
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence...",
+    },
+  },
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
-
+console.log(getBookDetails(booksArray[0]))
+console.log(getBookDetails(booksArray[1]))
+console.log(getBookDetails(booksArray[2]))
+console.log(getBookDetails(booksArray[3]))
 
 // Iteration 3 | Delete Language
-// Your code here:
+// Your code here:  //--------------quedamos eliminando los idiomas de todos los libros------
+for (let i = 0; i < booksArray.length; i++) {
+  let book = booksArray[i];
+  if (book.hasOwnProperty('details')) {
+    if (book.details.hasOwnProperty('language')) {
+     delete book.details.language;
+  }
+ }
+}
 
-
+console.log(booksArray);
 
 
 // Iteration 4 | Estimated Reading Time
