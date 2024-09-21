d2f74478f341ed75f5628c47518982ede12f05bb
RoniPG <ronigmn@gmail.com>
2024-09-21 11:10:46 +0200

    Iteration 2 | Book Details

--- src/books.js
+++ src/books.js
@@ -44,8 +44,9 @@ const booksArray = [
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
+  return `${book.title} - ${book.author} - ${book.pages} pages`
 
 }
 
