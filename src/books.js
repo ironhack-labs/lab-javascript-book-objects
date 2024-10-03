ff3dc25830246d585243c6750567da1ffc8c3a5a
Ignacio Molina <molinanacho33@gmail.com>
2024-10-03 19:52:53 +0200

    Finish lab

--- src/books.js
+++ src/books.js
@@ -1,52 +1,43 @@
 // Iteration 1 | Books Array
 
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
+class Book {
+    constructor(title, pages, author, details) {
+      this.title = title;
+      this.pages = pages;
+      this.author = author;
+      this.details = details;
+    }
+}
 
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
+class Details {
+  constructor(language, description) {
+    this.language = language;
+    this.description = description;
+  }
+}
 
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
+const book1 = new Book("The Old Man and the Sea", 128, "Ernest Hemingway",
+  new Details("English", "One of Hemingway's most famous works, it tells the story of Santiago..."));
 
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
+const book2 = new Book("The Airbnb Story", 256, "Leight Gallagher",
+  new Details("English", "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."));
 
+const book3 = new Book("Educated - A Memoir", 352, "Tara Westover",
+  new Details("English", "Educated is an account of the struggle for self-invention..."));
+
+const book4 = new Book("The Art of Learning", 288, "Josh Waitzkin",
+    new Details("English", "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."));
 
 // Your code here:
-const booksArray = [];
+const booksArray = [book1, book2, book3, book4];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
 
@@ -54,14 +45,24 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
+for(let i = 0; i < booksArray.length; i++) {
+  const book = booksArray[i];
 
+  delete(book.details.language);
+}
 
+console.log(booksArray);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
+for(let i = 0; i < booksArray.length; i++) {
+  const book = booksArray[i];
 
+  book["readingTime"] = Math.ceil((book.pages * 500) / 90);
+}
 
+console.log(booksArray);
 
 // Bonus: Iteration 5 | Books Dictionary
 
