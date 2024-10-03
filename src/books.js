f506fdb5757e2529ade4a3ffb347275213834d06
Chahinaz <crahmouni@uoc.edu>
2024-10-03 20:29:22 +0200

    Finish lab-javascript-book-objects

--- src/books.js
+++ src/books.js
@@ -1,64 +1,113 @@
 // Iteration 1 | Books Array
 
 // Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
+// title: "The Old Man and the Sea",
+// pages: 128,
+// author: "Ernest Hemingway",
 // details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
+//    language: "English",
+//    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
 // }
 
 // Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
+// title: "The Airbnb Story",
+// pages: 256,
+// author: "Leight Gallagher",
 // details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
+//    language: "English",
+//    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
 // }
 
 // Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
+// title: "Educated - A Memoir",
+// pages: 352,
+// author: "Tara Westover",
 // details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
+//    language: "English",
+//    description: "Educated is an account of the struggle for self-invention..."
 // }
 
 // Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
+// title: "The Art of Learning",
+// pages: 288,
+// author: "Josh Waitzkin",
 // details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
+//    language: "English",
+//    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
 // }
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leigh Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  }
+];
+
+console.log(booksArray);
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
 
 
 // Iteration 3 | Delete Language
-// Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language;
+}
+
+console.log(booksArray); 
+
 
 
 
 
 // Iteration 4 | Estimated Reading Time
-// Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  const book = booksArray[i];
+  const readingTime = Math.ceil((book.pages * 500) / 90);
+  book.readingTime = readingTime;
+}
+
+console.log(booksArray);
 
 
 
