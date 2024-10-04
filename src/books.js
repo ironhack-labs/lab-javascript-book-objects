befebfc9a8239f833e9546b9622eed8f47e5d904
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-04 18:03:24 +0200

    added missing book

--- src/books.js
+++ src/books.js
@@ -41,7 +41,7 @@
 const booksArray = [{title: "The Old Man and the Sea", pages: 128, author: "Ernest Hemingway", details:{language: "English",description:"One of Hemingway's most famous works, it tells the story of Santiago..."}},
   {title: "Educated - A Memoir", pages: 352, author: "Tara Westover", details:{language: "English",description:"Educated is an account of the struggle for self-invention..."}},
   {title: "The Art of Learning", pages: 288, author: "Josh Waitzkin", details:{language: "English",description:"The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."}},
-  {title: "Educated - A Memoir", pages: 352, author: "Tara Westover", details:{language: "English",description:"Educated is an account of the struggle for self-invention..."}}
+  {title: "The Airbnb Story", pages: 256, author: "Leight Gallagher", details:{language: "English",description:"This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."}}
 ];
 
 
