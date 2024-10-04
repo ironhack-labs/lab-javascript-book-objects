833a6b845b1291db9242bf9b7a39a4c8e744d167
Claudia Pando <claudiamfp@hotmail.com>
2024-10-04 15:54:38 +0100

    Solved Lab

--- src/books.js
+++ src/books.js
@@ -38,15 +38,57 @@
 
 
 // Your code here:
-const booksArray = [];
 
+
+
+const book1 = {
+  title: 'The Old Man and the Sea',
+  pages: 128,
+  author: 'Ernest Hemingway',
+  details: {
+    language: 'English',
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..." 
+    },
+  };
+
+  const book2 = {
+    title: 'The Airbnb Story',
+    pages: 256,
+    author: 'Leight Gallagher',
+    details: {
+        language: 'English',
+        description: 'This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...'
+     },  
+  };
+
+  const book3 = {
+  title: 'Educated - A Memoir',
+  pages: 352,
+  author: 'Tara Westover',
+  details: {
+      language: 'English',
+      description: 'Educated is an account of the struggle for self-invention...'
+ },
+  };
+
+  const book4 = {
+    title: 'The Art of Learning',
+    pages: 288,
+    author: 'Josh Waitzkin',
+    details: {
+      language: 'English',
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+     },    
+  };
+
+  const booksArray = [book1, book2, book3, book4];
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+return `${book.title} - ${book.author} - ${book.pages} pages`
 }
 
 
@@ -54,11 +96,25 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
+booksArray.forEach(book=> {
+  delete book.details.language;
+});
+console.log(booksArray);
+
+
+
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+
+
+booksArray.forEach(book=> {
+  book.readingTime = Math.round((book.pages*500)/90);
+});
+
+console.log(booksArray);
 
 
 
@@ -86,15 +142,23 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
   
+
+
 }
 
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(booksArray) {
   // Your code here:
-  
+
+  let totalPages = 0;
+
+  for(let i = 0; i< booksArray.length; i++){
+    totalPages += booksArray [i].pages ;
+}
+return totalPages / booksArray.length;
 }
