ae3ae93fcf2d39df2c093146fecbba06634140e6
Peedee01 <piethein@schouten.nl>
2024-10-20 15:25:50 +0200

    done

--- src/books.js
+++ src/books.js
@@ -1,65 +1,77 @@
 // Iteration 1 | Books Array
 
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
-
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
-
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
-
 // Your code here:
-const booksArray = [];
+const booksArray = [
+{
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+
+  {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+  language: "English",
+  description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+  }
+  },
+
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention...",
+    }
+},
+
+ {
+title: "The Art of Learning",
+pages: 288,
+author: "Josh Waitzkin",
+details: {
+     language: "English",
+     description: `The Art of Learning takes readers through Waitzkin's unique journey to excellence.
+     He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.`
+}
+}
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`
 }
 
-
-
 // Iteration 3 | Delete Language
-// Your code here:
 
+booksArray.forEach((book) => 
+  delete book.details.language
+)
+
+console.log(booksArray)
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
+  booksArray.forEach((book) => {
+  const totalWords = book.pages * 500;
+  const totalMinutes = totalWords / 90;
+  book.readingTime = Math.floor(totalMinutes);
+} 
+)
+console.log(booksArray);
 
 
 
@@ -68,6 +80,9 @@ function getBookDetails() {
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
+
+
+
 const dictionary = {
     "J. K. Rowling": [
         ["Harry Potter and the Philosopher's Stone", 223],
@@ -85,16 +100,32 @@ const dictionary = {
         ["Blink", 287],
     ],
 };
-
-function booksByAuthor() {
-  // Your code here:
-  
+function bookByAuthor (){
+  let booksArray = []
+  for (let author in dictionary){
+    dictionary[author].forEach((book) => {
+        const bookObject = {
+        title: book[0],
+        pages: book[1],
+        author: author,
+      }
+    booksArray.push(bookObject); 
+  }) 
 }
-
+return booksArray
+}
+console.log(bookByAuthor())
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
-  // Your code here:
-  
+function averagePageCount(booksArray) {
+  const sumAllPages = 0
+  booksArray.forEach((book) => {
+    
+    sumAllPages += book.pages
+  })
+  const averagePageCount = sumAllPages / booksArray.length
+  return averagePageCount
 }
+
+console.log(averagePageCount(bookByAuthor))
