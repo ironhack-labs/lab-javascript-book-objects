609a765f2ef7167e7aef08836068f3f4930204e6
Mr Footwork <pandau.ting@gmail.com>
2024-10-25 13:52:13 +0200

    setup

--- src/books.js
+++ src/books.js
@@ -36,32 +36,19 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
 // Your code here:
 const booksArray = [];
 
-
-
-
 // Iteration 2 | Book Details
 function getBookDetails() {
-  // Your code here:
-
+	// Your code here:
 }
-
-
 
 // Iteration 3 | Delete Language
 // Your code here:
 
-
-
-
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
-
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +56,28 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+	'J. K. Rowling': [
+		["Harry Potter and the Philosopher's Stone", 223],
+		['Harry Potter and the Chamber of Secrets', 251],
+		['Harry Potter and the Prisoner of Azkaban', 317],
+		['Harry Potter and the Goblet of Fire', 636],
+	],
+	'Neal Stephenson': [
+		['Cryptonomicon', 928],
+		['Anathem', 1008],
+		['Fall; or, Dodge in Hell', 896],
+	],
+	'Malcolm Gladwell': [
+		['Outliers', 320],
+		['Blink', 287],
+	],
 };
 
 function booksByAuthor() {
-  // Your code here:
-  
+	// Your code here:
 }
-
-
 
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
-  // Your code here:
-  
+	// Your code here:
 }
