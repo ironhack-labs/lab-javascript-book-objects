f1de4cb40a01c5ccf6a485b8227050967df9c5a9
Victor Mamani <victor.vikhal@gmail.com>
2024-10-26 09:08:24 +0200

    solved lab

--- src/books.js
+++ src/books.js
@@ -38,15 +38,67 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+   },
+
+   {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+   },
+
+   {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+   },
+
+   {
+    title: "The art of learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+   }
+];
 
 
 
 
-// Iteration 2 | Book Details
-function getBookDetails() {
+// Iteration 2 | Book 
+
+let myBook = {
+  title: "The art of learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+  }
+ }
+
+function getBookDetails(myBook) {
   // Your code here:
-
+    return `${myBook.title} - ${myBook.author} - ${myBook.pages} pages`
 }
 
 
@@ -54,11 +106,25 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
+  for (i=0; i<booksArray.length; i++){
+    delete booksArray[i].details.language
+  }
+  
+  console.log(booksArray[2])
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+
+for (i=0; i<booksArray.length; i++){
+  //let readingTime = (booksArray[i].pages * 500)/90
+
+  booksArray[i].readingTime = Math.ceil(booksArray[i].pages * 500/90);
+}
+
+console.log(booksArray[2])
+
 
 
 
