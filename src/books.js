e7424d56aa54e3c2d075c71a8996f40537c5a340
OlgaML <omolina@geekcowsd.com>
2025-01-17 17:57:56 +0100

    Solved Lab Olga

--- src/books.js
+++ src/books.js
@@ -38,29 +38,67 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {  title: 'The Old Man and the Sea' ,
+    pages: '128',
+    author: 'Ernest Hemingway',
+    details: {
+    language: 'English' ,
+    description: 'One of Hemingway \' s most famous works, it tells the story of Santiago...'
+  }
+},
+  {  title: 'The Airbnb Story',
+     pages: '256',
+     author: 'Leight Gallagher',
+     details: {
+        language: 'English',
+        description: 'This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...'
+     }
+  },
+  {  title: 'Educated - A Memoir',
+     pages: '352',
+     author: 'Tara Westover',
+     details: {
+        language: 'English',
+        description: 'Educated is an account of the struggle for self-invention...'
+     }
 
+  },
+  { title: 'The Art of Learning',
+    pages: '288',
+    author: 'Josh Waitzkin',
+    details: {
+        language: 'English',
+        description: 'The Art of Learning takes readers through Waitzkin\'s unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.'
+     }
 
+  }
+];
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+let book = '';
+function getBookDetails(book) {
   // Your code here:
+  
+    console.log( `${book.title} - ${book.author} - ${book.pages} pages`  )
 
 }
+booksArray.forEach ( getBookDetails )
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
 
-
+booksArray.forEach ( objArr => delete objArr.details.language);
+console.log(booksArray);
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
+booksArray.forEach ( objArr => objArr.readingTime = Math.ceil((objArr.pages * 500) / 90 ));
+console.log(booksArray);
 
 
 // Bonus: Iteration 5 | Books Dictionary
@@ -96,5 +134,12 @@ function booksByAuthor() {
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-  
-}
+  let totalPages = 0
+  booksArray.forEach( obj =>  {
+    totalPages += obj.pages
+  }  )
+            let bookPages = totalPages / booksArray.length;
+  return bookPages;
+} 
+ 
+ console.log(averagePageCount());
