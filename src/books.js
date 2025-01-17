703a8de2149797bcd82ce047c5fd587c9dc9f3db
Johana Castellano <johanacg16@gmail.com>
2025-01-17 16:10:44 +0000

    done without bonus

--- src/books.js
+++ src/books.js
@@ -1,66 +1,74 @@
 // Iteration 1 | Books Array
 
 // Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
+const book1 = {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+     language: "English",
+     description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+  }
+};
 
 // Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
+const book2 = {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+     language: "English",
+     description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+  }
+};
 
 // Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
+const book3 = {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+     language: "English",
+     description: "Educated is an account of the struggle for self-invention..."
+  }
+};
 
 // Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
+const book4 = {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+     language: "English",
+     description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+  }
+};
 
 
 // Your code here:
 const booksArray = [];
-
-
+booksArray.push(book1, book2, book3, book4);
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(bookObj) {
   // Your code here:
-
+  return `${bookObj.title} - ${bookObj.author} - ${bookObj.pages} pages`; 
 }
-
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
+booksArray.forEach(book => delete book.details.language);
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  booksArray[i].readingTime = Math.ceil((booksArray[i].pages * 500)/90);
+}
 
-
+console.log(booksArray);
 
 
 // Bonus: Iteration 5 | Books Dictionary
