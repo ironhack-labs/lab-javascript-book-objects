23550696f06716e698cc52ea003a6a008de38385
Fernando-Pucha <fernandopuchap@gmail.com>
2025-01-19 16:32:29 +0000

    done Fernando

--- src/books.js
+++ src/books.js
@@ -1,67 +1,68 @@
 // Iteration 1 | Books Array
 
 // Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
-
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
-
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
-
-// Your code here:
-const booksArray = [];
-
-
-
-
-// Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+const book1 = {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+  }
 }
 
 
+// Book 2
+const book2 = {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+  }
+}
+
+// Book 3
+const book3 = {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention..."
+  }
+}
+
+// Book 4
+const book4 = {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+  }
+}
+
+// Your code here:
+const booksArray = [];
+booksArray.push(book1, book2, book3, book4);
+
+// Iteration 2 | Book Details
+function getBookDetails(book) {
+  // Your code here:
+  let cadena = `${book.title} - ${book.author} - ${book.pages} pages`
+  return cadena
+}
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
-
+booksArray.forEach(book => delete book.details.language);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
-
+booksArray.forEach(book => book.readingTime = Math.ceil((book.pages * 500) / 90));
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +70,45 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
-  
+  const result = [];
+
+  for (const author in dictionary) {
+    dictionary[author].forEach(book => {
+      const bookObj = {
+        title: book[0],
+        pages: book[1],
+        author: author
+      };
+      console.log(bookObj)
+      result.push(bookObj);
+    });
+  }
+  return result;
 }
 
-
-
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(books) {
   // Your code here:
-  
+  const totalPages = books.reduce((sum, book) => sum + book.pages, 0);
+  const numberOfBooks = books.length;
+  return totalPages / numberOfBooks;
 }
