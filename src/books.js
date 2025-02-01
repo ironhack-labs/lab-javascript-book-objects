8ac11ec5e3f60eaa1b5d280d04baaf843a0535f7
znguye <dnguyen.jmblc@gmail.com>
2025-02-01 13:21:02 +0100

    book lab

--- src/books.js
+++ src/books.js
@@ -1,73 +1,76 @@
 // Iteration 1 | Books Array
-
-// Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
-
-// Book 2
-// title: The Airbnb Story
-// pages: 256
-// author: Leight Gallagher
-// details: {
-//    language: English
-//    description: This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...
-// }
-
-// Book 3
-// title: Educated - A Memoir
-// pages: 352
-// author: Tara Westover
-// details: {
-//    language: English
-//    description: Educated is an account of the struggle for self-invention...
-// }
-
-// Book 4
-// title: The Art of Learning
-// pages: 288
-// author: Josh Waitzkin
-// details: {
-//    language: English
-//    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
-// }
-
-
 // Your code here:
-const booksArray = [];
 
+const booksArray = [
+  {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+  }},
 
+  {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+  }},
+
+  {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention..."
+  }},
+
+  {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+  }}
+];
+
+console.log(typeof(booksArray[0].title)); //test
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+// Your code here:
+function getBookDetails(book) {
+  return (book.title + " - " + book.author + " - " + book.pages + " pages");
 }
 
-
+console.log(getBookDetails(booksArray[0])); //test
 
 // Iteration 3 | Delete Language
 // Your code here:
 
+booksArray.forEach(book => delete book.details.language);
 
+console.log(booksArray); //test
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
+booksArray.forEach(book => book.readingTime = Math.ceil(book.pages*500 / 90));
 
-
+console.log(booksArray); //test
 
 // Bonus: Iteration 5 | Books Dictionary
 
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
+
 const dictionary = {
     "J. K. Rowling": [
         ["Harry Potter and the Philosopher's Stone", 223],
@@ -86,15 +89,28 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
-  // Your code here:
-  
+// console.log(Object.keys(dictionary)); //test
+
+function booksByAuthor(dictionary) {
+  let booksByAuthorArray = [];
+  Object.keys(dictionary).forEach(author => {
+    dictionary[author].forEach(book => {
+      booksByAuthorArray.push({
+        title: book[0], 
+        pages: book[1], 
+        author: author});
+    });
+  });
+return booksByAuthorArray;
 }
 
-
+console.log(booksByAuthor(dictionary)); //test
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
-  // Your code here:
-  
+function averagePageCount(arr) {
+  let pageSum = 0;
+  arr.forEach(book => pageSum = pageSum + book.pages);
+  return pageSum / arr.length;
 }
+
+console.log(averagePageCount(booksArray)); //test
