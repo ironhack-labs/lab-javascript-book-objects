e87475eb0ab3f801f65824814b63bfa9a0c41da5
Santiago Villa <code@santiagovilla.it>
2025-02-17 21:46:36 +0100

    completed

--- src/books.js
+++ src/books.js
@@ -1,4 +1,4 @@
-// Iteration 1 | Books Array
+// // Iteration 1 | Books Array
 
 // Book 1
 // title: The Old Man and the Sea
@@ -37,28 +37,88 @@
 // }
 
 
+
 // Your code here:
-const booksArray = [];
+const booksArray = [
+
+  //book1
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+
+  //book2
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+
+   //book3
+   {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+
+     //book4
+     {
+      title: "The Art of Learning",
+      pages: 288,
+      author: "Josh Waitzkin",
+      details: {
+        language: "English",
+        description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+      }
+    }
+];
+
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
 
 
+
 // Iteration 3 | Delete Language
-// Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language;
+}
 
 
 
 
 // Iteration 4 | Estimated Reading Time
-// Your code here:
+// booksArray[1].readingTime = "string";
+// console.log(booksArray[1].pages);
+
+
+
+for (let i = 0; i < booksArray.length; i++){
+  const readingTime = (booksArray[i].pages * 500)/90;
+  booksArray[i].readingTime = Math.ceil(readingTime);
+  console.log(booksArray[i]);
+}
+
+
 
 
 
@@ -68,33 +128,111 @@ function getBookDetails() {
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
-const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
-};
+// const dictionary = {
+//     "J. K. Rowling": [
+//         ["Harry Potter and the Philosopher's Stone", 223],
+//         ["Harry Potter and the Chamber of Secrets", 251],
+//         ["Harry Potter and the Prisoner of Azkaban", 317],
+//         ["Harry Potter and the Goblet of Fire", 636],
+//     ],
+//     "Neal Stephenson": [
+//         ["Cryptonomicon", 928],
+//         ["Anathem", 1008],
+//         ["Fall; or, Dodge in Hell", 896],
+//     ],
+//     "Malcolm Gladwell": [
+//         ["Outliers", 320],
+//         ["Blink", 287],
+//     ],
+// };
 
-function booksByAuthor() {
-  // Your code here:
+// function booksByAuthor() {
+//   // Your code here:
   
-}
+// }
 
 
 
-// Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
-  // Your code here:
+// // Bonus: Iteration 6 | Average Page Count
+// function averagePageCount() {
+//   // Your code here:
   
-}
+// }
+
+
+// const fruitStore = {
+//   "tropical": [
+//     ["banana", 100]
+//     ["pineapple", 50]
+//   ],
+//   "european": [
+//     ["apple", 25]
+//     ["strawberry", 50]
+//   ]
+// }
+
+
+
+// for (let origin in fruitStore){
+//   console.log(`origin of fruits - ${origin}:`);
+
+//   const fruits = fruitStore[origin];
+  
+//   fruits.forEach(function(fruit){
+//     console.log(fruit);
+//   });
+  
+  
+// }
+
+// const library = {
+//   "J.K. Rowling": ["Harry Potter and the Philosopher's Stone", 325],
+//   "J.R.R. Tolkien": ["The Hobbit", "The Lord of the Rings"],
+//   "George Orwell": ["1984", "Animal Farm"]
+// };
+
+// for (let author in library) {
+//   console.log(`Books by ${author}:`);
+  
+//   // Access the array of books for this author
+//   const books = library[author];
+  
+//   // Loop through the array of books
+//   books.forEach(function(book) {
+//     console.log(book);
+//   });
+// }
+
+
+
+
+
+// let fruits = ['apple', 'banana', 'coconut', 'mango'];
+
+// for (let key in fruits){
+//   console.log(`${key}: ${fruits[key]}`);
+// }
+
+
+
+
+
+
+// const fruitByColor = [
+// {
+//   "fruitName": "banana",
+//   "price": 100
+// },
+// {
+//   "fruitName": "pineapple",
+//   "price": 50
+// }
+// ]
+
+
+
+
+
+let fruits = ["banana", "mango", "pear", "coconut"];
+
+console.log(fruits);
