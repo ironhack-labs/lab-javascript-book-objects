500a6d9f0a392663b9f736e80d0b40416fde2614
joaommj <joao_jacinto_8@hotmail.com>
2025-02-21 15:56:34 +0000

    day 5

--- src/books.js
+++ src/books.js
@@ -1,4 +1,4 @@
-// Iteration 1 | Books Array
+// // Iteration 1 | Books Array
 
 // Book 1
 // title: The Old Man and the Sea
@@ -38,28 +38,83 @@
 
 
 // Your code here:
-const booksArray = [];
+const book1 = {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+  }
+};
 
+const book2 = {
+  title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",  
+  details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+  }
+};
 
+const book3 = {
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention..."
+  }
+};
 
+const book4 = {
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+  }
+};
 
-// Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+const booksArray = [book1, book2, book3, book4];
+function getBooksArray() {
+  return booksArray;
 }
+
+
+
+
+// Iteration 2 | Book Details 
+function getBookDetails(book1) {
+  // Your code here:
+    // Return the formatted string for the given book
+    const result = `${book1.title} - ${book1.author} - ${book1.pages} pages`;
+    //return result;
+    return (result)
+  }
+
+  getBookDetails (book2);
+  
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
+for (let i=0; i < booksArray.length;i++){
+  const indexLang = booksArray[i];
+  delete indexLang.details.language;
+}
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
+for (let i=0; i < booksArray.length;i++){
+  const wichBook = booksArray[i];
+  wichBook.readingTime = Math.ceil((wichBook.pages * 500) / 90);
+}
 
 
 
