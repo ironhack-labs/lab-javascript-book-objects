e6f0528d19661247bfbd1ceff70608b0bdfd1f36
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-02-21 18:16:49 +0100

    done

--- src/books.js
+++ src/books.js
@@ -38,29 +38,48 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  { title: "The Old Man and the Sea", pages: 128, author: "Ernest Hemingway", details: 
+    { language: "English", description: "One of Hemingway's most famous works, it tells the story of Santiago..."}},
+  { title: "The Airbnb Story", pages: 256, author: "Leight Gallagher", details: 
+    {language: "English", description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."}},
+  { title: "Educated - A Memoir", pages: 352, author: "Tara Westover", details: 
+    { language: "English", description: "Educated is an account of the struggle for self-invention..."}},
+  { title: "The Art of Learning", pages: 288, author: "Josh Waitzkin", details: 
+    { language: "English", description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."}},
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  //console.log(`${booksArray.title} - ${booksArray.author} - ${booksArray.pages} pages`)
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
-
 
 
 // Iteration 3 | Delete Language
 // Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language;
+  console.log(booksArray[i]);
+}
 
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+for (let i = 0; i < booksArray.length; i++) {
+  const readTimeInMin = ((booksArray[i].pages * 500)/90);
 
-
+  booksArray[i].readingTime = Math.ceil(readTimeInMin);
+  //console.log(Math.ceil(readTimeInMin));
+  //console.log(booksArray);
+}
+/**/
 
 
 // Bonus: Iteration 5 | Books Dictionary
