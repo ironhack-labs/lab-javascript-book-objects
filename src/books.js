2a4efd0ea765df26995cee4b081cee4a7438f35e
ManuelLeal310 <manel.leal96@gmail.com>
2025-02-21 14:04:14 +0000

    solve books

--- src/books.js
+++ src/books.js
@@ -36,32 +36,73 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description:
+        "One of Hemingway's most famous works, it tells the story of Santiago, an old fisherman who struggles to catch a giant marlin in the Gulf Stream.",
+    },
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description:
+        "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb, one of the most successful and disruptive companies in the world.",
+    },
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention and the journey from growing up in a strict and abusive household in rural Idaho to earning a PhD from Cambridge University.",
+    },
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description:
+        "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    },
+  },
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
+function getBookDetails(title) {
+  const book = booksArray.find((book) => book.title === title);
 
+  // If the book is found, return its details; otherwise, return a message
+  if (book) {
+    return book.details;
+  } else {
+    return `Book with the title "${title}" not found.`;
+  }
 }
-
-
 
 // Iteration 3 | Delete Language
 // Your code here:
 
+for (let i = 0; i < booksArray.length; i++) {
+  delete booksArray[i].details.language;
+}
 
-
+console.log(booksArray);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
-
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +110,28 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
 function booksByAuthor() {
   // Your code here:
-  
 }
-
-
 
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-  
 }
