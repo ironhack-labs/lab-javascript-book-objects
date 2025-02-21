1201ac991f5078098f429a50f8513bf1fa1827fd
Akshaya Srinivas <akshaya.pra12@gmail.com>
2025-02-21 15:10:39 +0100

    Solved lab

--- src/books.js
+++ src/books.js
@@ -38,15 +38,55 @@
 
 
 // Your code here:
-const booksArray = [];
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+ pages: 128,
+  author: "Ernest Hemingway",
+ details: {
+   language: "English",
+  description: "One of Hemingway's most famous works, it tells the story of Santiago",
+ }
+},
+{
+title: "The Airbnb Story",
+ pages: 256,
+ author: "Leight Gallagher",
+ details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb",
+ }
+},
+{
+// Book 3
+ title: "Educated - A Memoir",
+ pages: 352,
+ author: "Tara Westover",
+ details: {
+    language: "English",
+    description: "Educated is an account of the struggle for self-invention",
+ }
+},
+{
+// Book 4
+ title: "The Art of Learning",
+ pages: 288,
+ author: "Josh Waitzkin",
+ details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure",
+}
+}
+];
+console.log(booksArray);
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(bookObject) {
   // Your code here:
-
+ 
+return `${bookObject.title} - ${bookObject.author} - ${bookObject.pages} pages`;
 }
 
 
@@ -54,13 +94,18 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
-
-
+booksArray.forEach(book=>{
+  if(book.details && book.details.language){
+    delete book.details.language;
+  }
+});
+console.log(booksArray);
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
+booksArray.forEach(book=>{
+  book.readingTime=Math.ceil((book.pages *500)/90);
+})
 
 
 // Bonus: Iteration 5 | Books Dictionary
