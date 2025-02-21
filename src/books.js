1ef1c6688722ff30db4d03047ef3a058527d4505
Rojda Polat <rojdapolat@outlook.de>
2025-02-21 14:58:57 +0100

    done

--- src/books.js
+++ src/books.js
@@ -38,28 +38,82 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+  }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+  }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention...",
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    }
+  },
+
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
+console.log(getBookDetails(booksArray[3]));
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
+function deleteLanguage(booksArrayLan) {
+  for (let i = 0; i < booksArrayLan.length; i++) {
+    delete booksArrayLan[i].details.language;
+  }
+  return booksArrayLan;
+}
 
+console.log(deleteLanguage(booksArray));
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
+function addingProperty(arrayWithBooks) {
+  for (let i = 0; i < arrayWithBooks.length; i++) {
+    arrayWithBooks[i].readingTime = Math.ceil((arrayWithBooks[i].pages * 500) / 90);
+  }
+  return arrayWithBooks;
+}
+
+console.log(addingProperty(booksArray));
 
 
 
