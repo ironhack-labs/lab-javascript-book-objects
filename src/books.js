e9a27dc48398727a2d3e01fbc6681b102d1d544c
CannyRo <whatcannydev@gmail.com>
2025-02-21 14:46:35 +0100

    Solved by Ronan CANNY

--- src/books.js
+++ src/books.js
@@ -36,32 +36,68 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description:
+        "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description:
+        "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description:
+        "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  }
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
-
-
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
-
+booksArray.forEach( book => {
+  delete book.details.language;
+})
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
-
+booksArray.forEach( book => {
+  const estimatedDuration = (book.pages * 500 / 90);
+  book.readingTime = Math.ceil(estimatedDuration);
+})
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +105,46 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
-  
+  const bookCollectin = [];
+  for( const authorName in dictionary) {
+    dictionary[authorName].forEach( bookInfo => {
+      const newBook = {
+          title: bookInfo[0],
+          pages: bookInfo[1],
+          author: authorName
+      };
+      bookCollectin.push(newBook);
+    });
+  }
+  return bookCollectin;
 }
 
-
-
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(books) {
   // Your code here:
-  
+  sumOfPages = 0;
+  books.forEach( book => {
+    sumOfPages += book.pages;
+  });
+  const averageNumberOfPages = sumOfPages / books.length;
+  return averageNumberOfPages;
 }
