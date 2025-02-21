e3787180eb3f818c1c3f8e4b90f4a10e3c1a869b
Sosha Fedotovskaia <sinterma@gmail.com>
2025-02-21 14:48:36 +0100

    books

--- src/books.js
+++ src/books.js
@@ -1,5 +1,7 @@
 // Iteration 1 | Books Array
 
+
+
 // Book 1
 // title: The Old Man and the Sea
 // pages: 128
@@ -38,27 +40,70 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {
+      title: "The Old Man and the Sea",
+      pages: 128,
+      author: "Ernest Hemingway",
+      details: {
+          language: "English",
+          description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+      }
+  },
+  {
+      title: "The Airbnb Story",
+      pages: 256,
+      author: "Leight Gallagher",
+      details: {
+          language: "English",
+          description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+      }
+  },
+  {
+      title: "Educated - A Memoir",
+      pages: 352,
+      author: "Tara Westover",
+      details: {
+         language: "English",
+          description: "Educated is an account of the struggle for self-invention..."
+      }
+  },
+  {
+      title: "The Art of Learning",
+      pages: 288,
+      author: "Josh Waitzkin",
+      details: {
+          language: "English",
+          description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+      }
+  }
+];
+
+console.log(booksArray);
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
-
-
+booksArray.forEach(book => {
+  delete book.details.language;
+});
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+booksArray.forEach(book => {
+  book.readingTime = Math.ceil((book.pages * 500) / 90);
+});
+
+console.log(booksArray);
 
 
 
@@ -87,6 +132,7 @@ const dictionary = {
 };
 
 function booksByAuthor() {
+   
   // Your code here:
   
 }
